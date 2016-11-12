7ebd8554eda26aefa3bb39aa31496a90679cf4f7
Catalin Ionescu <catalin.ionescu282@gmail.com>
2016-11-12 14:44:41 +0000

    [rspec] Add a spec for the participants grouping method

--- spec/participants_spec.rb
+++ spec/participants_spec.rb
@@ -1,6 +1,21 @@
 require "participants"
 
 RSpec.describe Participants do
-  context "forever_alone" do
+  context "group_participants" do
+    let(:participants) { [1, 2, 3] }
+
+    it "groups the first participant with the second" do
+      group = subject.group_participants(participants).first
+
+      expect(group[:from]).to eq 1
+      expect(group[:to]).to eq 2
+    end
+
+    it "groups the last participant with the first" do
+      group = subject.group_participants(participants).last
+
+      expect(group[:from]).to eq 3
+      expect(group[:to]).to eq 1
+    end
   end
 end
